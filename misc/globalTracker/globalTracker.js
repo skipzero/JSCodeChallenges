f06aa054df1598d30cfa420a2353415c66707dfb
Skip McBride <skip@zerosquadron.com>
2015-12-02 19:18:11 -0800

    added index.html for testing

--- misc/globalTracker/globalTracker.js
+++ misc/globalTracker/globalTracker.js
@@ -3,11 +3,11 @@
 let globalTracker = {
 	start : function() {
 		let startingArray = Object.keys( window );
+		return startingArray;
 	},
 
 	change : function() {
 		let changedArray = Object.keys( window );
 	}
 }
-
 console.log(globalTracker.start())
